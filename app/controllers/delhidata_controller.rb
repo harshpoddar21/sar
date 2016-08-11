abe0e226a107d7f06f345910adb0f4cc47c91d5e
harsh poddar <harsh.poddar@shuttl.com>
2016-08-11 14:52:05 +0530

    Added line details

--- app/controllers/delhidata_controller.rb
+++ app/controllers/delhidata_controller.rb
@@ -45,4 +45,40 @@ class DelhidataController < ApplicationController
 
   end
 
+
+  def computePerpendiularDistance
+    allData=Array.new
+
+    CSV.foreach("/var/www/Ruby/sar/public/mindist.csv", :headers => true) do |row|
+
+      allData.push row
+
+    end
+    distance=Array.new
+
+    allData.each do |data1|
+      puts data1[0]
+
+      allData.each do |data2|
+        if data1[0]!=data2[0] && LocationUtil.distance([data1[1].to_f,data1[2].to_f],[data2[1].to_f,data2[2].to_f])<3000 && LocationUtil.perpendicularDistance([data1[1].to_f,data1[2].to_f],[data1[3].to_f,data1[4].to_f],[data2[1].to_f,data2[2].to_f],[data2[3].to_f,data2[4].to_f])<=50
+
+          distance.push([data1[0],data2[0]])
+
+        else
+
+#          distance.push ([data1[0],data2[0],100000000000])
+
+        end
+
+      end
+      break
+
+    end
+
+    CSV.open( "/var/www/Ruby/sar/public/perpendFin.csv", 'w' ) do |writer|
+      distance.each do |c|
+        writer << c
+      end
+    end
+  end
 end
--- lib/utility/location_util.rb
+++ lib/utility/location_util.rb
@@ -15,4 +15,35 @@ module LocationUtil
 
     rm * c # Delta in meters
   end
+
+  def self.perpendicularDistance loc1, loc2,loc3,loc4
+
+    diffInLng=97756
+    diffInLat=111194
+    x1=loc1[0]*diffInLat
+    x2=loc2[0]*diffInLat
+    y1=loc1[1]*diffInLng
+    y2=loc2[1]*diffInLng
+    x3=loc3[0]*diffInLat
+    x4=loc4[0]*diffInLat
+    y3=loc3[1]*diffInLng
+    y4=loc4[1]*diffInLng
+    area1=0.5*(x1*y2 + x2*y3 + x3*y1 - x2*y1 - x3*y2 - x1*y3)
+    area1=area1.abs
+    area2=0.5*(x1*y2 + x2*y4 + x4*y1 - x2*y1 - x4*y2 - x1*y4)
+    area2=area2.abs
+    base1=Math.sqrt((x2-x1)**2+(y2-y1)**2)
+
+    base2=base1
+
+    height1=area1*2/base1
+    height2=area2*2/base2
+
+    return height1>height2 ? height2:height1
+
+
+  end
+
+
+
 end
