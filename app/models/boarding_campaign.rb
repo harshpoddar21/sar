b9b90c7465e85483da9ad5c912c0a52d5b905551
harsh poddar <harsh.poddar@shuttl.com>
2017-01-02 12:07:54 +0530

    Fixed bug for bit ly

--- app/models/boarding_campaign.rb
+++ app/models/boarding_campaign.rb
@@ -14,7 +14,7 @@ class BoardingCampaign
 
     response= "Hi We are very excited to help you in making your office commute hassle free. If"
     +" you would like to book Shuttl click #{BitlyUtils.shortenUrl("http://urbanmetro.in/book_shuttl?data="+Utils.generateBase64(from,to,channelCategory,channelId,campaignId,phoneNumber))}"
-      +". To stop click #{BitlyUtils.shortenUrl("+http://urbanmetro.in/unsubscribe?data="+Utils.generateBase64(from,to,channelCategory,channelId,campaignId,phoneNumber))}"
+      +". To stop click #{BitlyUtils.shortenUrl("http://urbanmetro.in/unsubscribe?data="+Utils.generateBase64(from,to,channelCategory,channelId,campaignId,phoneNumber))}"
 
     response
 
