89ecb20787930c010303a1aa1b2e9cd36b7d406d
harsh poddar <harsh.poddar@shuttl.com>
2017-01-02 11:54:51 +0530

    Fixed bug for message

--- app/models/boarding_campaign.rb
+++ app/models/boarding_campaign.rb
@@ -12,10 +12,11 @@ class BoardingCampaign
 
   def self.generateBoardingMessageForCampaign from,to,channelCategory,channelId,campaignId,phoneNumber
 
-    return "Hi We are very excited to help you in making your office commute hassle free. If"
-      +"you would like to book Shuttl click #{BitlyUtils.shortenUrl("http://urbanmetro.in/book_shuttl?data="+Utils.generateBase64(from,to,channelCategory,channelId,campaignId,phoneNumber))}"
+    response= "Hi We are very excited to help you in making your office commute hassle free. If"
+    +" you would like to book Shuttl click #{BitlyUtils.shortenUrl("http://urbanmetro.in/book_shuttl?data="+Utils.generateBase64(from,to,channelCategory,channelId,campaignId,phoneNumber))}"
       +". To stop click #{BitlyUtils.shortenUrl("+http://urbanmetro.in/unsubscribe?data="+Utils.generateBase64(from,to,channelCategory,channelId,campaignId,phoneNumber))}"
 
+    response
 
   end
 
