d2688a6a5e5ab12575cef8d738e871d44413d78c
harsh poddar <harsh.poddar@shuttl.com>
2017-01-09 00:10:02 +0530

    Added google url Shortener

--- lib/utility/bitly_utils.rb
+++ lib/utility/bitly_utils.rb
@@ -3,9 +3,21 @@ module BitlyUtils
   USERNAME="harshpoddar21"
   API_KEY="R_ad270eebc6624243b3141ae410861728"
   def self.shortenUrl url
-    bitly = Bitly.new(USERNAME, API_KEY)
-    result=bitly.shorten url
-    result.short_url
+    short=nil
+    begin
+
+      bitly = Bitly.new(USERNAME, API_KEY)
+      result=bitly.shorten url
+      short=result.short_url
+
+    rescue Exception => error
+
+      short=Googl.shorten(url,"","AIzaSyDwY1YUoIsaJl6f9ouaUZCiCGekccqjkkQ").short_url
+
+    end
+    short
+
+
   end
 
 
